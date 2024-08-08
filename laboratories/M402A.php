8a8a68f4c2aab13b18cfcf884fd36b90ed52e7e5
jnvlnzl <102937800+jnvlnzl@users.noreply.github.com>
2024-08-09 07:44:50 +0800

    Add files via upload

--- laboratories/M402A.php
+++ laboratories/M402A.php
@@ -32,7 +32,7 @@ $items = $conn->query($sql);
             <nav class="navbar navbar-expand-lg fixed-top">
                 <div class="container">
                     <a class="navbar-brand" href="../home.html">
-                        <i class="fas fa-robot"></i>ECOR
+                      <img src="../assets/logo.png" alt="ECOR Logo" class="navbar-logo">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
